593afe3ae01b5871d12bf9407afbd4b626e29ac2
JacksonSummers66 <59920714+JacksonSummers66@users.noreply.github.com>
2020-02-24 14:22:48 -0500

    Updated the LICENSE and made the requested changes

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -3,7 +3,16 @@ extends Node2D
 onready var Camera = $Camera
 
 func _ready():
-	pass
+	if Input.is_action_pressed("Left"):
+		add_trauma(10)
+	if Input.is_action_pressed("Right"):
+		add_trauma(10)
+	if Input.is_action_pressed("Up"):
+		add_trauma(10)
+	if Input.is_action_pressed("Down"):
+		add_trauma(10)
 
 func _physics_process(delta):
-	pass
+	$Camera.add_trauma(0.1)
+	
+	
